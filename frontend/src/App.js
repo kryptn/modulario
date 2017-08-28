8a0cd76c6e0a9184570039576d24b20487a5fee4
kryptn <kryptn@gmail.com>
2017-08-28 00:12:41 -0600

    first pass through, can view a post now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Fetch from 'react-fetch-component'
 import {
   BrowserRouter as Router,
   Route,
@@ -52,26 +53,43 @@ const Topics = ({ match }) => (
 )
 
 
+const CreatePost = ({ match }) => (
+  <div> words </div>
+)
+
+const ViewPost = ({ match }) => (
+  <div>
+    <Fetch url={`http://localhost:5000/api/v1/view/${match.params.postId}`}>
+      {({ loading, data, error }) => (
+        <div>
+          {loading && <span>Loading...</span>}
+          {data && <div><pre>{JSON.stringify(data, null, 2) }</pre></div>}
+        </div>
+      )}
+      </Fetch>
+    </div>
+)
+
+
 class App extends Component {
   render() {
     return (
-
-
       <Router>
-   <div>
-     <ul>
-       <li><Link to="/">Home</Link></li>
-       <li><Link to="/about">About</Link></li>
-       <li><Link to="/topics">Topics</Link></li>
-     </ul>
+         <div>
+           <ul>
+             <li><Link to="/">Home</Link></li>
+             <li><Link to="/view">View Post</Link></li>
+             <li><Link to="/create">Create Post</Link></li>
+           </ul>
 
-     <hr/>
+           <hr/>
 
-     <Route exact path="/" component={Home}/>
-     <Route path="/about" component={About}/>
-     <Route path="/topics" component={Topics}/>
-   </div>
- </Router>
+           <Route exact path="/" component={Home}/>
+           <Route path="/create" component={CreatePost}/>
+           <Route path="/view/:postId" component={ViewPost}/>
+
+         </div>
+       </Router>
     );
   }
 }
